db4f20399eb2d507b35d6fd6c85b2a6eb8ce3f5c
Your Name <qn@ubuntu.(none)>
2013-03-10 12:18:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppOfTheCentury::Application.config.secret_token = 'ec291b7ab19d5650c954ddbc4ea3aaa03c037fe20b697e45d3ffc64d0c0d4605a5b3188686a8d6c2b89e615fa627c9d5a8d6682c97d3771b4cdc2ce6807d1c25'
